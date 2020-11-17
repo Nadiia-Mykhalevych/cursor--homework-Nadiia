929f9838745f28ce3eaf8f24e1d85b5a9660f2fb
Nadiia-Mykhalevych <nadiia.mykhalevych7@gmail.com>
2020-11-17 18:06:18 +0200

    homework #2 added

---
+++ HW#2 Цикли і розгалуження/main.js
@@ -0,0 +1,19 @@
+let firstNumber = +prompt('Введіть перше число:');
+while (Number.isNaN(firstNumber) ){
+    firstNumber = +prompt('Введіть ціле перше число:');
+}
+let lastNumber = +prompt('Введіть друге число:');
+
+
+while (Number.isNaN(lastNumber) ){
+    lastNumber = +prompt('Введіть ціле друге число:');
+}
+const skipEven = confirm ('Потрібно пропускати парні числа?');
+
+
+let ResultSum = 0;
+for(let i = firstNumber; i <= lastNumber; i++){
+    if (skipEven === true && i%2 === 0 ) continue;
+        ResultSum += i;
+    }
+console.log('Сума чисел = ' + ResultSum);
